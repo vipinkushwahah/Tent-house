f73eeb9427d1665a129f5050dbccc128a3341bfb
Vipin Kushwaha <vipinkushwaha@Vipins-MacBook-Air.local>
2025-02-01 22:57:52 +0530

    fixed-logo

--- src/components/qr/qr.js
+++ src/components/qr/qr.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { QRCodeCanvas } from 'qrcode.react';
+import { QRCode } from "react-qrcode-logo";
+import logo from '../assates/logo.jpeg';
 
 const QRCodeComponent = () => {
   const url = "https://maa-bhawani-tent-house.netlify.app";
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '20px' }}>
-      <h2>Scan the QR Code to Visit the Website</h2>
-      <QRCodeCanvas value={url} size={200} />
-      <p>{url}</p>
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1 style={{ fontSize: "24px", fontWeight: "bold", marginBottom: "20px" }}>
+        Scan the QR Code to Open the URL
+      </h1>
+
+      <div
+        style={{
+          display: "inline-block",
+          padding: "20px",
+          background: "#fff",
+          borderRadius: "15px",
+          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <QRCode
+          value={url}
+          size={280} // QR Code size
+          logoImage={logo}
+          logoWidth={60} // Proportional size
+          logoHeight={60}
+          logoOpacity={1}
+          logoPadding={10}
+          removeQrCodeBehindLogo={false}
+        />
+      </div>
+
+      <p style={{ marginTop: "10px", fontSize: "18px", fontWeight: "bold" }}>{url}</p>
     </div>
   );
 };
